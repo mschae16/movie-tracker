3c8694851dfca2465082c3618ad1adf83b4acaee
David Becker <david@beckersite.com>
2017-08-30 13:58:26 -0600

    Added Router functionality for Redux

--- app/components/app/app.js
+++ app/components/app/app.js
@@ -1,20 +1,20 @@
 import React from 'react';
-import LoginContainer from '../../containers/loginContainer'
-import MovieIndexContainer from '../../containers/movieIndexContainer'
+import LoginContainer from '../../containers/loginContainer';
+import MovieIndexContainer from '../../containers/movieIndexContainer';
 // import CreateUser from '../createuser/createuser';
-import { Route, NavLink, Link } from 'react-router-dom';
+import { Route } from 'react-router';
 
 const App = () => {
-  return (
-    <div>
-    <LoginContainer />
-    <MovieIndexContainer />
-    </div>
-  )
-}
+	return (
+		<div>
+			<Route exact path="/login" component={LoginContainer} />
+			<Route exact path="/" component={MovieIndexContainer} />
+		</div>
+	);
+};
 
-  // <Route exact path='/login' component={ Login } />
-  // <Route exact path='/' render={ ()=> <MovieIndex movieData={ movieData }/>} />
-  // <Route exact path='/createuser' component={ CreateUser } />
+// <Route exact path='/login' component={ Login } />
+// <Route exact path='/' render={ ()=> <MovieIndex movieData={ movieData }/>} />
+// <Route exact path='/createuser' component={ CreateUser } />
 
 export default App;
