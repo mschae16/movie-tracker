6ed647511bcb41c3dab562fb63d6cb287d0a9a08
Margo Schaedel <margoschaedel@gmail.com>
2017-08-31 15:43:18 -0600

    Add conditional rendering for login createuser and home page

--- app/components/navheader/NavHeader.js
+++ app/components/navheader/NavHeader.js
@@ -1,32 +1,43 @@
-import React, { Component } from 'react';
-import { NavLink } from 'react-router-dom';
+import React, { Component } from "react";
+import { NavLink } from "react-router-dom";
 
 export default class NavHeader extends Component {
-	render() {
-		const { loginSuccess, createUserSuccess } = this.props
-		console.log(this.props);
-		if (createUserSuccess === 'success' || loginSuccess === 'success') {
-			console.log('success sign out');
-      return (
-        <button>Sign Out</button>
-      )
-    }
+  render() {
+    const { loginSuccess, createUserSuccess } = this.props;
 
     return (
       <div>
         <section className="navigation-wrapper">
-					<div className="slide-right">
-          	<header className="movie-tracker-header">MovieTracker</header>
-					</div>
+          <div className="slide-right">
+            <header className="movie-tracker-header">MovieTracker</header>
+          </div>
           <p> Hello NAME </p>
-					<p> Favorites </p>
+          <p> Favorites </p>
           <p>Logout</p>
 
-          <NavLink activeClassName="selected" className="nav" to="/">Home</NavLink>
+          <NavLink activeClassName="selected" className="nav" to="/">
+            Home
+          </NavLink>
 
-          <NavLink activeClassName="selected" className="nav" to="/login">Login</NavLink>
+          {loginSuccess !== "success" && createUserSuccess !== 'success' && (
+            <NavLink activeClassName="selected" className="nav" to="/login">
+              Login
+            </NavLink>
+          )}
 
-          <NavLink activeClassName="selected" className="nav" to="/createuser">CreateUser</NavLink>
+          {createUserSuccess !== "success" && loginSuccess !== 'success' && (
+            <NavLink
+              activeClassName="selected"
+              className="nav"
+              to="/createuser"
+            >
+              CreateUser
+            </NavLink>
+          )}
+
+          {(createUserSuccess === "success" || loginSuccess === "success") && (
+            <button>Sign Out</button>
+          )}
         </section>
       </div>
     );
