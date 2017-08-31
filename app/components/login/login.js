3b7507c2e7d824a2ff287a5308c6ecc337fd106b
Laura Caroselli <lcaroselli@outlook.com>
2017-08-31 17:16:48 -0600

    Start log-in page

--- app/components/login/login.js
+++ app/components/login/login.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Redirect } from 'react-router';
+import { Redirect } from "react-router";
 
 class Login extends Component {
   constructor() {
@@ -26,34 +26,35 @@ class Login extends Component {
   render() {
     const { handleLoginSubmit } = this.props;
 
-		if (this.props.loginSuccess === 'success') {
-  		return (
-  			<Redirect to="/" />
-  		)
-  	}
-
+    if (this.props.loginSuccess === "success") {
+      return <Redirect to="/" />;
+    }
 
     return (
       <div>
         <form
+          className="login-form"
           onSubmit={e => {
             e.preventDefault();
             this.constructUserObject();
           }}
         >
           <input
+            className="login-input"
             type="text"
             placeholder="Email"
             value={this.state.email}
             onChange={e => this.setState({ email: e.target.value })}
           />
           <input
+            className="login-input"
             type="password"
             placeholder="Password"
             value={this.state.password}
             onChange={e => this.setState({ password: e.target.value })}
           />
-          <input type="submit" value="Login" />
+          <input className="login" type="submit" value="Login" />
+          <h4 className="login-message">Dont have a login? <span className="create-user">Create user.</span></h4>
         </form>
       </div>
     );
