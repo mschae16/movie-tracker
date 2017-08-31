89cc082a2eb5e6e267916754a69e906c64640ad0
David Becker <david@beckersite.com>
2017-08-31 11:37:46 -0600

    Add NavHeader component with NavLinks

--- app/components/app/app.js
+++ app/components/app/app.js
@@ -2,18 +2,18 @@ import React from 'react';
 import LoginContainer from '../../containers/loginContainer';
 import MovieIndexContainer from '../../containers/movieIndexContainer';
 import CreateUserContainer from '../../containers/createUserContainer';
+import NavHeader from '../navheader/NavHeader';
 import { Route } from 'react-router';
 
 const App = () => {
-
 	return (
 		<div>
-      <Route exact path="/createuser" component={CreateUserContainer} />
-      <Route exact path="/login" component={LoginContainer} />
-      <Route exact path="/" component={MovieIndexContainer} />
+			<NavHeader />
+			<Route exact path="/createuser" component={CreateUserContainer} />
+			<Route exact path="/login" component={LoginContainer} />
+			<Route exact path="/" component={MovieIndexContainer} />
 		</div>
 	);
 };
-
 
 export default App;
